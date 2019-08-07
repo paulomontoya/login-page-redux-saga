e53b8e9f394b80d46c77902d89c69a87fa7f0141
Paulo Montoya <paulo@paulomontoya.com.br>
2019-08-07 11:06:33 -0300

    Add redux-persist to user state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import { Route, Switch } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
-import store, { history } from "./store";
+import { store, persistor, history } from "./store";
 import DashboardPage from "./components/DashboardPage";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <div className={css.App}>
       <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Switch>
-            <Route path="/" exact={true} component={LoginPage} />
-            <Route path="/dashboard" component={DashboardPage} />
-          </Switch>
-        </ConnectedRouter>
+        <PersistGate loading={null} persistor={persistor}>
+          <ConnectedRouter history={history}>
+            <Switch>
+              <Route path="/" exact={true} component={LoginPage} />
+              <Route path="/dashboard" component={DashboardPage} />
+            </Switch>
+          </ConnectedRouter>
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -3,20 +3,34 @@ import createSagaMiddleware from "redux-saga";
 import { createBrowserHistory } from "history";
 import { routerMiddleware } from "connected-react-router";
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import createRootReducer from "./reducers";
 import rootSaga from "./sagas";
 
 export const history = createBrowserHistory();
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  createRootReducer(history),
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"]
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  createRootReducer(history)
+);
+
+export const store = createStore(
+  persistedReducer,
   compose(
     applyMiddleware(routerMiddleware(history)),
     applyMiddleware(sagaMiddleware)
   )
 );
+export const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
-
-export default store;
